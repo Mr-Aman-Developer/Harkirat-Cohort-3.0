cdfb1f1addd22a9cb4fe8c316d773af73009113b
Aman Bhardwaj <Aman71204@hotmail.com>
2025-08-12 21:41:41 +0530

    Update object-2.js

--- object-2.js
+++ object-2.js
@@ -4,7 +4,7 @@
 function greet(user){
 console.log("Hi"+ user.gender + user.name+", your Age is "+ user.age);
 if (user.age>18){
-console.log("Hi"+ user.gender + user.name+", your Age is "+ user.age);
+console.log("you are ");
 }
 }
 let user= {
