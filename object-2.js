ef7766acc2f7e2198971771ad0e76329ad6c4713
Aman Bhardwaj <Aman71204@hotmail.com>
2025-08-12 21:39:31 +0530

    Update object-2.js

--- object-2.js
+++ object-2.js
@@ -2,7 +2,7 @@
 //and gender and greets the user with their gender
 // (Hi Mr/Mrs/Others harkirat, your age is 21)
 function greet(user){
-cosnole.log("Hi"+ user.gender + user.name+", your Age is "+ user.age);
+console.log("Hi"+ user.gender + user.name+", your Age is "+ user.age);
 }
 let user= {
 name:"Aman",
