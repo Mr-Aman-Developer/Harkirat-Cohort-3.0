e119851d71149523311feba81f9095b75290f150
Aman Bhardwaj <Aman71204@hotmail.com>
2025-08-12 21:31:45 +0530

    Update object-2.js

--- object-2.js
+++ object-2.js
@@ -1,2 +1,3 @@
 //Write a function that takes a new object as input which has name , age  
-and gender and greets the user with their gender (Hi Mr/Mrs/Others harkirat, your age is 21)
+and gender and greets the user with their gender
+// (Hi Mr/Mrs/Others harkirat, your age is 21)
