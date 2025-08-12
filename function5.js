30de39b6c13c76756655a34689f8c5e37d8992de
Aman Bhardwaj <Aman71204@hotmail.com>
2025-08-12 20:06:23 +0530

    Update function5.js

--- function5.js
+++ function5.js
@@ -1,8 +1,8 @@
 //Write a function called sum that finds the sum from 1 to a num
-function sum (a){
-let first=0;
-for (let i=0;i<a;i++){
-first= first+i;
+function sum(a) {
+  let first = 0;
+  for (let i = 0; i < a; i++) {
+    first = first + i;
+  }
 }
-}
-console.log(num);
+console.log();
