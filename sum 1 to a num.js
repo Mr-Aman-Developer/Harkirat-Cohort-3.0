2065e4e8eae3fe7cb3534c8c412da9a23bfeb795
Aman Bhardwaj <Aman71204@hotmail.com>
2025-08-13 21:07:13 +0530

    Update sum 1 to a num.js

--- sum 1 to a num.js
+++ sum 1 to a num.js
@@ -1,10 +1,10 @@
 function sum(a) {
   let num = 0;
-  for (let i = 1; i <=a; i++) {
+  for (let i = 0; i < a; i++) {
     num = num + a;
   }
   return num;
 }
 
-const num = sum(100);
+const num = sum(3);
 console.log(num);
